68636cfaccd983b94898c1d4ee4e270f83057987
ayshaElb <umayoub78@gmail.com>
2020-05-18 13:46:22 +0200

    implementation of react application

---
+++ assets/js/app.js
@@ -0,0 +1,33 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+// les imports IMPORTANT
+ import React from 'react';
+ import ReactDOM from 'react-dom';
+
+// any CSS you import will output into a single css file (app.css in this case)
+import '../css/app.css';
+import Navbar from './components/Navbar';
+import HomePage from './pages/Homepage';
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
+// import $ from 'jquery';
+
+console.log('Hello Webpack Encore!!!!');
+
+const App = () => {
+    return <>
+        <Navbar />
+
+        <div class="container pt-5"><HomePage />
+
+        </div>
+     </>
+};
+
+
+const rootElement = document.querySelector('#app');
+ReactDOM.render(<App />, rootElement);
